c13dfd75bb39e31e213cd9f64c183a14f9049ef3
Ever Arteaga <togeinumaki1227@gmail.com>
2025-07-29 12:19:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,37 +6,37 @@ const productos = [
         id: 1,
         nombre: "Dorsay Class",
         descripcion: "90 ml", // Puedes mantener el precio aquí si lo necesitas para otras lógicas, solo no se mostrará.
-        imagen: "Dorsay.JPEG"
+        imagen: "img/Dorsay.JPEG"
     },
     {
         id: 2,
         nombre: "Men",
         descripcion: "200 ml",
-        imagen: "MEN.JPEG"
+        imagen: "img/MEN.JPEG"
     },
     {
         id: 3,
         nombre: "Liasson",
         descripcion: "50 ml",
-        imagen: "Liasson.JPEG"
+        imagen: "img/Liasson.JPEG"
     },
     {
         id: 4,
         nombre: "Dancing",
         descripcion: "90 ml",
-        imagen: "Dancing.JPEG"
+        imagen: "img/Dancing.JPEG"
     },
     {
         id: 5,
         nombre: "Mithyka",
         descripcion: "50 ml",
-        imagen: "Mithyka.JPEG"
+        imagen: "img/Mithyka.JPEG"
     },
     {
         id: 6,
         nombre: "Bleu Femmu Capri",
         descripcion: "50 ml",   
-        imagen:"Bleu Femmu Capri.JPEG"
+        imagen:"img/Bleu Femmu Capri.JPEG"
     },
     {
         id: 7,
@@ -48,13 +48,13 @@ const productos = [
         id: 8,
         nombre: "Bleu Night",
         descripcion: "100 ml",
-        imagen:"Bleu Night.JPEG"
+        imagen:"img/Bleu Night.JPEG"
     },
      {
         id: 9,
         nombre: "Cardigan",
         descripcion: "90 ml",
-        imagen:"Cardigan.JPEG"
+        imagen:"img/Cardigan.JPEG"
     },
      {
         id: 10,
@@ -66,73 +66,73 @@ const productos = [
         id: 11,
         nombre: "Expression",
         descripcion: "50 ml",
-        imagen:"Expression.JPEG"
+        imagen:"img/Expression.JPEG"
     },
      {
         id: 12,
         nombre: "Mini Chics",
         descripcion: "120 ml",
-        imagen:"Mini Chics.JPEG"
+        imagen:"img/Mini Chics.JPEG"
     },
     {
         id: 13,
         nombre: "New Cool",
         descripcion: "75 ml",
-        imagen:"New Cool.JPEG"
+        imagen:"img/New Cool.JPEG"
     },
     {
         id: 14,
         nombre: "Nitro",
         descripcion: "100 ml",
-        imagen:"Nitro.JPEG"
+        imagen:"img/Nitro.JPEG"
     },
     {
         id: 15,
         nombre: "Pack Mithyca",
         descripcion: "50 ml / 130 ml",
-        imagen:"Pack Mithyka.JPEG"
+        imagen:"img/Pack Mithyka.JPEG"
     },
     {
         id: 16,
         nombre: "Prints",
         descripcion: "30 ml",
-        imagen:"Prints.JPEG"
+        imagen:"img/Prints.JPEG"
     },
     {
         id: 17,
         nombre: "Splash refrescantes",
         descripcion: "200 ml",
-        imagen:"Splash refrescantes.JPEG"
+        imagen:"img/Splash refrescantes.JPEG"
     },
     {
         id: 18,
         nombre: "Splash Universe",
         descripcion: "200 ml",
-        imagen:"Splash Universe.JPEG"
+        imagen:"img/Splash Universe.JPEG"
     },
     {
         id: 19,
         nombre: "Talco",
         descripcion: "230 gr",
-        imagen:"Talco.JPEG"
+        imagen:"img/Talco.JPEG"
     },
     {
         id: 20,
         nombre: "Vanilla",
         descripcion: "50 ml",
-        imagen:"Vanilla.JPEG"
+        imagen:"img/Vanilla.JPEG"
     },
     {
         id: 21,
         nombre: "Vibranza",
         descripcion: "45 ml",
-        imagen:"Vibranza.JPEG"
+        imagen:"img/Vibranza.JPEG"
     }, 
     {
         id: 22,
         nombre: "sweet black",
         descripcion: "50 ml",
-        imagen:"Sweet black.JPEG"
+        imagen:"img/Sweet black.JPEG"
     }, 
 ];
 
